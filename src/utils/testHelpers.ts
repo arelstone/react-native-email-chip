2723b27599d23031636243065385e7fa2f0d9372
Carsten Daurehøj <arelstone@gmail.com>
2022-03-30 11:07:29 +0200

    cleanup

--- src/utils/testHelpers.ts
+++
@@ -1,5 +0,0 @@
-import { ShallowWrapper } from 'enzyme';
-
-export const findElementByTestId = (Component: any, testID: string, wrapper: ShallowWrapper) => {
-    return wrapper.find(Component).findWhere((n: any) => n.prop('testID') === testID);
-};
